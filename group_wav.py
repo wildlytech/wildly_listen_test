a4eeccb16257634938c325534a3b19f2fd823e4b
ShivarajMeti <shivaraj.meti002@gmail.com>
2019-05-06 15:56:44 +0530

    made changes to read the extra header information of wav file by just adding tag to list

--- group_wav.py
+++ group_wav.py
@@ -5,19 +5,22 @@ from ftplib import FTP
 from io import BytesIO
 from ssl import SSLSocket
 import argparse
+import csv
 
 
 DESCRIPTION = 'Input The path to the directory'
 HELP = 'Input the path'
 PARSER = argparse.ArgumentParser(description=DESCRIPTION)
 PARSER.add_argument('-input_path', '--input_path', action='store',
-                    help=HELP, default='/home/user-u0xzU/')
+                    help=HELP)
 
 RESULT = PARSER.parse_args()
 PRIMARY_PATH = "/home/user-u0xzU" + RESULT.input_path
 print PRIMARY_PATH
 
-'''https://stackoverflow.com/questions/53143518/get-files-names-inside-a-zip-file-on-ftp-server
+'''
+Code is copied from the below link
+https://stackoverflow.com/questions/53143518/get-files-names-inside-a-zip-file-on-ftp-server
 -without-downloading-whole-archiv#53144697'''
 #Code of class FTPFile is taken from the above link
 
@@ -72,69 +75,78 @@ class FtpFile:
 
 #To connect to FTP server
 def connect():
-	''' TO connect to ftp server
+    ''' TO connect to ftp server
 	'''
-	global ftp, CURRENT_PATH, NAMES
-	ftp = FTP('******', user='******', passwd='******')
-	print "connected to FTP"
-	ftp.cwd(PRIMARY_PATH)
-	CURRENT_PATH = ftp.pwd()#assingn the current path to a variable y
-	NAMES = ftp.nlst()#get the list of all files
+    global ftp, CURRENT_PATH, NAMES
+    ftp = FTP('*****', user='*****', passwd='****')
+    print "connected to FTP"
+    ftp.cwd(PRIMARY_PATH)
+    CURRENT_PATH = ftp.pwd()
+    NAMES = ftp.nlst()
 
 
 def group_wav_files():
-	''' function to group wav files
-	'''
+    '''
+    function to group wav files
+    '''
+    # varible to keep the count of no.of files
+    tag_names = ["Filename", "Operator", "DeviceID", "Battery_Voltage", "Battery_Percentage"]
+    name_of_devices = set()
+    initial_number_of_devices = 0
+    count = 0
 
-	count = 0#varible to keep the count of no.of files
-	for name in NAMES:
-		print name
+    with open("wav_file_extra_information.csv", "w") as file_object:
+        wav_information_object = csv.writer(file_object)
+        wav_information_object.writerow(tag_names)
+        file_object.flush()
 
-		if (name[-3:] == 'wav') or (name[-3:] == 'WAV'):#check if the file is wav file only
-			fh_1 = BytesIO(FtpFile(ftp, name).read(128))
-			#Get the header of the wav file
+        for name in NAMES:
+            print name
+            if (name[-3:] == 'wav') or (name[-3:] == 'WAV'):#check 	if the file is wav file only
+                file_header_info = BytesIO(FtpFile(ftp, name).read(128))
 
-			riff, size, fformat = struct.unpack('<4sI4s', fh_1.read(12))
-			chunk_header = fh_1.read(8)
-			subchunkid, subchunksize = struct.unpack('<4sI', chunk_header)
-			aformat, channels, samplerate, byterate, blockalign, bps = struct.unpack('HHIIHH', fh_1.read(16))
-			chunkoffset = fh_1.tell()
-			fh_1.seek(chunkoffset)
+                # Get the header of the wav file
+                riff, size, fformat = struct.unpack('<4sI4s', file_header_info.read(12))
+                chunk_header = file_header_info.read(8)
+                subchunkid, subchunksize = struct.unpack('<4sI', chunk_header)
+                aformat, channels, samplerate, byterate, blockalign, bps = struct.unpack('HHIIHH', file_header_info.read(16))
+                chunkoffset = file_header_info.tell()
+                file_header_info.seek(chunkoffset)
+                subchunk2id, subchunk2size = struct.unpack('<4sI', file_header_info.read(8))
+                listtype = struct.unpack('<4s', file_header_info.read(4))
+                listitemid, listitemsize = struct.unpack('<4sI', file_header_info.read(8))
 
-			subchunk2id, subchunk2size = struct.unpack('<4sI', fh_1.read(8))
 
-			listtype = struct.unpack('<4s', fh_1.read(4))
+                # Getting the wav information and writing the csv file rows
+                listdata = file_header_info.read(listitemsize)
+                wav_information = listdata.decode("ascii").split(',')
+                
+                information_value = [name]
+                for index_value, each_tag_value in enumerate(wav_information):
+                    _, corresponding_value = each_tag_value.split(":")
+                    if index_value == 0:
+                        name_of_devices.add(corresponding_value)
+                        destination = CURRENT_PATH+"/"+corresponding_value +"/"
+                        file_path_source = CURRENT_PATH + "/" + name
+                        if corresponding_value not in ftp.nlst():
+                            ftp.mkd(destination)
+                            ftp.rename(file_path_source, destination + "/" + name)
+                            count += 1
+                        else:
+                            ftp.rename(file_path_source, destination + "/" + name)
+                            count += 1
+                            print "No.of file moved ", count
 
-			listitemid, listitemsize = struct.unpack('<4sI', fh_1.read(8))
-			listdata = fh_1.read(listitemsize)
-			#read the Header that is added manually
-			print " real information: %s " %(listdata.decode("ascii"))
-			#split the list by :
-			li_1 = listdata.decode("ascii").split(':')
-			i = li_1.index('Operator')#get the index of operator
-			print "This is Operator Name ::", li_1[i+1].split(',')[0]#print the operator name
-			print "Device Id ::", li_1[i+2].split(',')[0]#get the device id
+                    information_value.append(corresponding_value)
+                print information_value
+                wav_information_object.writerow(information_value)
+                file_object.flush()
 
-			device_id = li_1[i+2].split(',')[0]
+                if len(name_of_devices) > initial_number_of_devices:
+                    print "New Device Started Uploading", name_of_devices
+                    initial_number_of_devices = len(name_of_devices)
 
-			print name, device_id#Print the name of wav files and device id
-			destination = CURRENT_PATH+"/"+device_id +"/"#Destination path
-			print destination
-			file_path_source = CURRENT_PATH + "/" + name
-			print file_path_source
-			if device_id not in ftp.nlst():
-				#if the directory does not exist make a directory
-				ftp.mkd(destination)
-				#move the wav file to the destination direc
-				ftp.rename(file_path_source, destination + "/" + name)
-				count += 1
-				print "directory created"
-			else:
-				#if the directory already exists just move the file to that directory
-				ftp.rename(file_path_source, destination + "/" + name)
-				count += 1
-				print "No.of file moved ", count
 if __name__ == '__main__':
-	connect()
-	group_wav_files()
+    connect()
+    group_wav_files()
 
