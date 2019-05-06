1fbfcc46d351fc2a05621cc3c850bb97d8f8fcb5
ShivarajMeti <shivaraj.meti002@gmail.com>
2019-05-06 12:02:52 +0530

    removed the double usage of variable, cleaning code, removing unnecessary comments

--- ftp_status.py
+++ ftp_status.py
@@ -4,8 +4,6 @@ from ftplib import FTP
 import argparse
 
 
-
-
 DESCRIPTION = 'Input The path to the directory'
 HELP = 'Input the path'
 PARSER = argparse.ArgumentParser(description=DESCRIPTION)
@@ -18,15 +16,17 @@ print PRIMARY_PATH
 
 
 def connect():
-    '''TO connect to ftp
+    '''
+    TO connect to ftp
     '''
     global ftp, CURRENT_DIR
-    ftp = FTP('********', user='****', passwd='******')
+    ftp = FTP('*****', user='****', passwd='****')
     print "connected to FTP"
     CURRENT_DIR = ftp.cwd(PRIMARY_PATH)
 
 def last_modified():
-    '''to count the total no.of files and last modified file in a directory
+    '''
+    To count the total no.of files and last modified file in a directory
     '''
     names = ftp.nlst()
     latest_time = None
@@ -34,13 +34,12 @@ def last_modified():
     count = 0
 
     for name in names:
-    	if (name[-3:] == 'wav') or (name[-3:] == 'WAV'):
-    	    time1 = ftp.voidcmd("MDTM " + name)
-            print name
+        if (name[-3:] == 'wav') or (name[-3:] == 'WAV'):
+            time1 = ftp.voidcmd("MDTM " + name)
             count += 1
             if (latest_time is None) or (time1 > latest_time):
-    	        latest_name = name
-    	        latest_time = time1
+                latest_name = name
+                latest_time = time1
 
     print "Last uploaded file is", latest_name, latest_time[4:]
     print "Total no.of files is :", count
@@ -51,18 +50,13 @@ def download_files(flag):
     ftp server
     """
     if flag == 1:
-        # ftp.cwd('/wav_file/')
-        # print ftp.nlst()
         ex = ftp.nlst()
-        ex = []
+        ex_wav = []
         wav_file_count = 0
-        # ex = [wav_file for wav_file in ex if wav_file[-3:]=='wav' or 'WAV']
         for wav in ex:
             if (wav[-3:] == 'wav') or (wav[-3:] == 'WAV'):
-                ex.append(wav)
-        # with open('list_of_files.pkl', 'rb') as file_obj:
-        #     old_ex = pickle.load(file_obj)
-        for each_file in ex:
+                ex_wav.append(wav)
+        for each_file in ex_wav:
             if each_file[-3:] == 'wav' or 'WAV':
                 try:
                     wav_file_count = wav_file_count + 1
@@ -81,14 +75,14 @@ def clearing(flag):
     '''to delete the files from FTP
     '''
     if flag == 1:
-		lis = ftp.nlst()
-		for filename in lis:
-			print "file deleted ", filename
-			ftp.delete(filename)
+        lis = ftp.nlst()
+        for filename in lis:
+            print "file deleted ", filename
+            ftp.delete(filename)
 
 
 if __name__ == '__main__':
     connect()
     last_modified()
-    download_files(1)
-    clearing(1)
+    TOTAL_FILES_DOWNLOADED = download_files(1)
+    # clearing(1)
