0c0b73dce1a497323a3ce075e06c305b6ff8f93a
srinivas <srinivasreddyh>
2019-05-20 17:03:02 +0530

    checks file size and then groups files based on device ID

--- group_wav.py
+++ group_wav.py
@@ -89,44 +89,50 @@ def group_wav_files():
     function to group wav files
     '''
     # varible to keep the count of no.of files
-    tag_names = ["Filename", "Operator", "DeviceID", "Battery_Voltage",
-                 "Battery_Percentage", "Network_status", "Firmare_Revision", "Time_Stamp"]
+    global name_of_devices
+    NAMES = ftp.nlst()
     name_of_devices = set()
     initial_number_of_devices = 0
     count = 0
 
-    with open("wav_file_extra_information.csv", "w") as file_object:
-        wav_information_object = csv.writer(file_object)
-        wav_information_object.writerow(tag_names)
-        file_object.flush()
+    for name in NAMES:
+        # print name
+        if (name[-3:] == 'wav') or (name[-3:] == 'WAV'):#check  if the file is wav file only
+            file_header_info = BytesIO(FtpFile(ftp, name).read(264))
 
-        for name in NAMES:
-            print name
-            if (name[-3:] == 'wav') or (name[-3:] == 'WAV'):#check 	if the file is wav file only
-                file_header_info = BytesIO(FtpFile(ftp, name).read(172))
+            # Get the header of the wav file
+            riff, size, fformat = struct.unpack('<4sI4s', file_header_info.read(12))
+            chunk_header = file_header_info.read(8)
+            subchunkid, subchunksize = struct.unpack('<4sI', chunk_header)
+            aformat, channels, samplerate, byterate, blockalign, bps = struct.unpack('HHIIHH', file_header_info.read(16))
+            chunkoffset = file_header_info.tell()
+            file_header_info.seek(chunkoffset)
+            subchunk2id, subchunk2size = struct.unpack('<4sI', file_header_info.read(8))
+            listtype = struct.unpack('<4s', file_header_info.read(4))
+            listitemid, listitemsize = struct.unpack('<4sI', file_header_info.read(8))
 
-                # Get the header of the wav file
-                riff, size, fformat = struct.unpack('<4sI4s', file_header_info.read(12))
-                chunk_header = file_header_info.read(8)
-                subchunkid, subchunksize = struct.unpack('<4sI', chunk_header)
-                aformat, channels, samplerate, byterate, blockalign, bps = struct.unpack('HHIIHH', file_header_info.read(16))
-                chunkoffset = file_header_info.tell()
-                file_header_info.seek(chunkoffset)
-                subchunk2id, subchunk2size = struct.unpack('<4sI', file_header_info.read(8))
-                listtype = struct.unpack('<4s', file_header_info.read(4))
-                listitemid, listitemsize = struct.unpack('<4sI', file_header_info.read(8))
-
-                # Getting the wav information and writing the csv file rows
-                listdata = file_header_info.read(172)
-                wav_information = listdata.decode("ascii").split(',')
-                information_value = [name]
-                for index_value, each_tag_value in enumerate(wav_information):
-                    try:
-                        _, corresponding_value = each_tag_value.split(":")
-                    except ValueError:
-                        corresponding_value = "".join(each_tag_value.split(":")[1:])
-                    if index_value == 0:
-                        name_of_devices.add(corresponding_value)
+            # Getting the wav information and writing the csv file rows
+            listdata = file_header_info.read(200)
+            # print "List data", listdata
+            wav_information = listdata.decode("ascii").split(',')
+            information_value = [name]
+            for index_value, each_tag_value in enumerate(wav_information):
+                try:
+                    _, corresponding_value = each_tag_value.split(":")
+                except ValueError:
+                    corresponding_value = "".join(each_tag_value.split(":")[1:])
+                if index_value == 1:
+                    name_of_devices.add(corresponding_value)
+                    # Switch to Binary mode
+                    ftp.sendcmd("TYPE i")
+                    # Get size of file
+                    filesize = ftp.size(name)
+                    # To switch back to ASCII
+                    ftp.sendcmd("TYPE A")
+                    if filesize < (samplerate*blockalign*10):
+                        count += 1
+                    else:
+                        #moving files based on device ID
                         destination = CURRENT_PATH+"/"+corresponding_value +"/"
                         file_path_source = CURRENT_PATH + "/" + name
                         if corresponding_value not in ftp.nlst():
@@ -138,13 +144,10 @@ def group_wav_files():
                             count += 1
                             print "No.of file moved ", count
 
-                    information_value.append(corresponding_value)
-                wav_information_object.writerow(information_value)
-                file_object.flush()
-
-                if len(name_of_devices) > initial_number_of_devices:
-                    print "New Device Started Uploading", name_of_devices
-                    initial_number_of_devices = len(name_of_devices)
+            if len(name_of_devices) > initial_number_of_devices:
+                print "New Device detected :", name_of_devices
+                initial_number_of_devices = len(name_of_devices)      
+    return destination
 
 if __name__ == '__main__':
     connect()
