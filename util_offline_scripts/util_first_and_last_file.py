00d033b2630059287c11a646d957d8720721f940
srinivasreddyh <shrini071@gmail.com>
2019-08-29 18:20:03 +0530

    this script gives first and last wav file with timestamp

---
+++ util_offline_scripts/util_first_and_last_file.py
@@ -0,0 +1,51 @@
+import argparse
+import glob
+import os
+import struct
+import util_offline
+
+DESCRIPTION = 'Input The path to the directory'
+HELP = 'Input the path'
+PARSER = argparse.ArgumentParser(description=DESCRIPTION)
+PARSER.add_argument('-input_path', '--input_path', action='store', help='Input path to wav files')
+PARSER.add_argument('-input_first_character', '--input_first_character', action='store', \
+	help='Input required first character of the file name series')
+
+RESULT = PARSER.parse_args()
+PATH = RESULT.input_path
+CHARACTER = RESULT.input_first_character
+print "Path:", PATH
+print "character:", CHARACTER
+
+os.chdir(PATH)
+files_list = glob.glob(str(CHARACTER)+'*')
+# print "files:", files_list
+
+def first_and_last_modified_files():
+	#sorting files
+	wavfiles_list = []
+	sorted_wav_files_list = util_offline.sort_on_timestamp(files_list)
+
+	# get wavheader and extraheader
+	for each_wav_file in sorted_wav_files_list:
+		try:
+			wav_header, extra_header = util_offline.get_wavheader_extraheader(each_wav_file)
+			blockalign = wav_header["BlockAlign"]
+			samplerate = wav_header["SampleRate"]
+
+			if util_offline.check_wav_file_size(each_wav_file, samplerate, blockalign):
+				for index_value, each_tag_value in enumerate(extra_header):
+					timestamp_tag = extra_header[index_value].split(":", 1)[0]
+					if timestamp_tag == ' Timestamp':
+						timestamp_value = extra_header[index_value].split(":", 1)[1]
+						wavfiles_list.append((each_wav_file, str(timestamp_value)))
+		except struct.error:
+			continue
+
+	first_and_last_modified_files = wavfiles_list[0], wavfiles_list[-1]
+	return first_and_last_modified_files
+
+if __name__ == '__main__':
+	first_and_last_modified_files = first_and_last_modified_files()
+	print "First file:", first_and_last_modified_files[0]
+	print " Last file:", first_and_last_modified_files[-1]
